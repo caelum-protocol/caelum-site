5769d69c2ec6b0a579ff07a16a9e48511f73617d
Caelum Protocol <Founder@caelumprotocol.org>
2025-07-03 22:01:09 -0500

    New Build Fixes

--- next.config.js
+++ next.config.js
@@ -1,28 +1,34 @@
-// next.config.js
-const webpack = require('webpack');
+const webpack = require("webpack");
+const withTM = require("next-transpile-modules")(["viem"]); // required for viem compatibility on Vercel
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // Polyfills for Node.js core modules used by Web3/Irys SDKs
       config.resolve.fallback = {
         ...config.resolve.fallback,
-        fs: false, // Do NOT try to bundle fs in browser
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        os: require.resolve('os-browserify/browser'),
-        path: require.resolve('path-browserify'),
-        buffer: require.resolve('buffer/'),
-        process: require.resolve('process/browser'),
+        fs: false,
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        os: require.resolve("os-browserify/browser"),
+        path: require.resolve("path-browserify"),
+        buffer: require.resolve("buffer/"),
+        process: require.resolve("process/browser"),
       };
+
       config.plugins.push(
         new webpack.ProvidePlugin({
-          Buffer: ['buffer', 'Buffer'],
-          process: ['process'],
+          Buffer: ["buffer", "Buffer"],
+          process: ["process"],
         })
       );
     }
+
     return config;
   },
 };
+
+module.exports = withTM(nextConfig);
+
